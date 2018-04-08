3e0093b3a225964c8e4c8efebc2c7dfa617a52fe
sherylynn <352281674@qq.com>
2018-04-08 17:24:09 +0800

    初始化提交

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # annie-wsl-tools
 add powerful bookmarks for download video  by annie directly on windows
+
+NEED TO DO
+
+install debian in wsl && apt install ffmpeg &&apt install golang:  && go get github.com/iawia002/annie 
---
+++ install.bat
@@ -0,0 +1,12 @@
+﻿@echo off
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+@echo off
+cls
+setlocal EnableDelayedExpansion
+rem %~fs0是执行文件 %~dp0是文件夹
+Reg add HKCR\annie /ve /t REG_SZ /d "URL:annie Protocol Handler" /f
+Reg add HKCR\annie /v "URL Protocol" /t REG_SZ /f
+Reg add HKCR\annie\shell\open\command /ve  /t REG_SZ /d "%~dp0you-get-tools.bat \"%%1\"" /f
+echo test
+pause
