0afcd27d2e019543f44bbdffca8ddabac6c813a9
sherylynn <352281674@qq.com>
2018-04-09 10:06:10 +0800

    初步能用了

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ NEED TO DO
 
 install debian in wsl && apt install ffmpeg &&apt install golang:  && go get github.com/iawia002/annie && sudo ln -s $HOME/go/bin/annie /usr/bin/annie
 为了让wsl获取到annie，需要作一个软链接
+
+和原来的youget-tools相比
+bookmarklet.js就去掉了原来为了匹配多加的各种""冒号
+比原来的更简洁了，并且通过替换wsl的内容，应该可以实现直接的使用windows版本的annie
--- annie-wsl-tools.bat
+++ annie-wsl-tools.bat
@@ -2,49 +2,25 @@
 cls
 setlocal EnableDelayedExpansion
 
-rem echo dev
-
-set env=%~dp0
-set path=%path%;%env%
-set path=%path%;%env%\deps\ffmpeg\bin
-set path=%path%;%env%\deps\rtmpdump
 echo %1
 set url=%1
 set DownPath=%USERPROFILE%\Downloads
 
-rem set env=%AppData%\..\Local\Google\Chrome\User Data\Default\Extensions\iealjklbjaieahkhjemfjfggdcghmhad\1.0_0
-if %url:~0,10% EQU "annie://" (
+if %url:~1,8% EQU annie:// (
     echo link from chrome
-    if %url:~10,1% EQU 1 (
+    if %url:~9,1% EQU 1 (
       set proxy=-x "127.0.0.1:10808"
       set itag=
-      set url=%url:~11,-1%
+      set url=%url:~10,-1%
     )
-    if %url:~10,1% EQU 0 (
+    if %url:~9,1% EQU 0 (
       set proxy=
       set itag=
-      set url=%url:~11,-1%
+      set url=%url:~10,-1%
       goto :AnnieDown
     )
     
   )
-
-
-::for /f "tokens=2,3 skip=3 delims= " %%a in ('you-get %proxy% -i %url%') do (
-::  echo %%a
-::  if %%a EQU itag: (
-::    set itag= --itag=%%b
-::    echo skip 4 line ,break_loop when first loop 
-::    goto :AnnieDown
-::  )
-::)
-
-rem echo 没考虑到有些youtube没有title 不能直接skip 4行
-rem for /f "tokens=3 skip=4 delims= " %%a in ('you-get -x %proxy% -i %url') do (
-rem   set itag= %%a
-rem   echo skip 4 line ,break_loop when first loop 
-rem   goto :AnnieDown
-rem )
 
 :AnnieDown
 echo I don't know if I need itag anymore
