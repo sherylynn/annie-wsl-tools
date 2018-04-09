a5345c80d7dc096087a751859a22382e9a2a0016
sherylynn <352281674@qq.com>
2018-04-09 08:28:47 +0800

    稍微适配了下annie

--- annie-wsl-tools.bat
+++ annie-wsl-tools.bat
@@ -10,42 +10,45 @@ set path=%path%;%env%\deps\ffmpeg\bin
 set path=%path%;%env%\deps\rtmpdump
 echo %1
 set url=%1
+set DownPath=%USERPROFILE%\Downloads
 
 rem set env=%AppData%\..\Local\Google\Chrome\User Data\Default\Extensions\iealjklbjaieahkhjemfjfggdcghmhad\1.0_0
-set DownPath=%USERPROFILE%\Downloads
-if %url:~0,11% EQU "youget://" (
+if %url:~0,10% EQU "annie://" (
     echo link from chrome
-    if %url:~11,1% EQU 1 (
-      set proxy=-x "127.0.0.1:1080"
-      set url=%url:~12,-1%
+    if %url:~10,1% EQU 1 (
+      set proxy=-x "127.0.0.1:10808"
+      set itag=
+      set url=%url:~11,-1%
     )
-    if %url:~11,1% EQU 0 (
+    if %url:~10,1% EQU 0 (
       set proxy=
       set itag=
-      set url=%url:~12,-1%
-      goto :YouGetGet
+      set url=%url:~11,-1%
+      goto :AnnieDown
     )
     
   )
 
 
-for /f "tokens=2,3 skip=3 delims= " %%a in ('you-get %proxy% -i %url%') do (
-  echo %%a
-  if %%a EQU itag: (
-    set itag= --itag=%%b
-    echo skip 4 line ,break_loop when first loop 
-    goto :YouGetGet
-  )
-)
+::for /f "tokens=2,3 skip=3 delims= " %%a in ('you-get %proxy% -i %url%') do (
+::  echo %%a
+::  if %%a EQU itag: (
+::    set itag= --itag=%%b
+::    echo skip 4 line ,break_loop when first loop 
+::    goto :AnnieDown
+::  )
+::)
 
 rem echo 没考虑到有些youtube没有title 不能直接skip 4行
 rem for /f "tokens=3 skip=4 delims= " %%a in ('you-get -x %proxy% -i %url') do (
 rem   set itag= %%a
 rem   echo skip 4 line ,break_loop when first loop 
-rem   goto :YouGetGet
+rem   goto :AnnieDown
 rem )
 
-:YouGetGet
-echo %itag% top itag is bigest not default in you-get readme
-you-get %proxy% %itag% -o %DownPath%  %url%
+:AnnieDown
+echo I don't know if I need itag anymore
+::wsl annie %proxy% %itag% -o %DownPath%  %url%
+cd %DownPath%
+wsl annie %proxy% %itag% %url%
 pause
